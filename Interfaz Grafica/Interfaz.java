2d9f8fbb8ea75e94a3680f673a0cb9d691ca82f0
emarojass05 <e84671818@gmail.com>
2023-09-26 22:24:29 -0600

    commit

--- Interfaz Grafica/Interfaz.java
+++ Interfaz Grafica/Interfaz.java
@@ -3,10 +3,17 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
 import javax.swing.*;
 import javax.imageio.ImageIO;
 import java.io.IOException;
+import java.awt.event.MouseAdapter;
+import java.util.ArrayList;
+import java.util.List;
+import java.io.DataOutputStream;
+import java.net.Socket;
+
 
 public class Interfaz {
 
@@ -57,7 +64,6 @@ public class Interfaz {
             }
         });
         milamina.add(playbtn);
-
         JButton infobtn = new JButton();
         infobtn.setBounds(525, 200, 150, 50);
         infobtn.setText("INFO");
@@ -112,42 +118,467 @@ class Ventana2 extends JFrame {
             }
         });
         milaminagame.add(returnbtn);
+
+
+
+        JButton gamebtn = new JButton();
+        gamebtn.setBounds(470, 200, 150 , 30);
+        gamebtn.setText("GAME");
+        gamebtn.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                new Ventanagame();
+                dispose();
+            }
+
+        });
+        
+
+        milaminagame.add(gamebtn);
+
         setVisible(true);
     }
 }
 
 class Ventana3 extends JFrame {
-    public Ventana3(){
+    private BufferedImage backgroundImage;
+
+    public Ventana3() {
         setLayout(null);
         setTitle("Informacion");
         setSize(1200, 675);
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel milaminainfo = new JPanel();
-        milaminainfo.setLayout(null);
-        milaminainfo.setSize(1200,675);
-        milaminainfo.setBackground(Color.black);
+        try {
+            // Cargar la imagen de fondo
+            backgroundImage = ImageIO.read(getClass().getResource("instrucciones.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        JLabel infor = new JLabel();
-        infor.setText("<html>Este proyecto fue elaborado por:<br>Jimmy Feng Feng<br>Gabriel Fernandez Vargas<br>y Emanuel Rojas Fernandez</html>");
-        infor.setBounds(400,10,600,200);
-        infor.setFont(new Font("MV Boli",Font.PLAIN,30));
-        infor.setForeground(Color.white);
-        milaminainfo.add(infor);
+        JPanel milaminainfo = new JPanel() {
+            @Override
+            protected void paintComponent(Graphics g) {
+                super.paintComponent(g);
+                // Dibuja la imagen de fondo
+                g.drawImage(backgroundImage, 0, 0, getWidth(), getHeight(), this);
+            }
+        };
+        milaminainfo.setLayout(null);
+        milaminainfo.setSize(1200, 675);
 
         JButton returnbtni = new JButton();
-        returnbtni.setBounds(20, 550, 100 , 30);
+        returnbtni.setBounds(20, 600, 100, 30);
         returnbtni.setText("RETURN");
-        returnbtni.addActionListener(new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                new Interfaz();
-                dispose();
-            }
+        returnbtni.addActionListener(e -> {
+            new Interfaz();
+            dispose();
         });
         milaminainfo.add(returnbtni);
 
         add(milaminainfo);
+
         setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            new Ventana3();
+        });
+    }
+}
+
+
+class Ventanagame extends JFrame{
+
+    public Ventanagame(){
+        setLayout(null);
+        setSize(1000,675);
+        setTitle("CONNECT THE DOTS");
+        setResizable(false);
+        
+
+        PanelDePuntos panelDePuntos = new PanelDePuntos(10, 10);
+        panelDePuntos.setBounds(0, 0, 1200, 675);
+
+        add(panelDePuntos);
+
+        setVisible(true);
+
+    }
+
+}
+
+class PanelDePuntos extends JPanel{
+    private ListaEnlazada<Punto> puntosTotales = new ListaEnlazada<>();
+    private List<Punto> puntosSeleccionados = new ArrayList<>();
+    private List<Linea> lineasDibujadas = new ArrayList<>();
+    private ListaEnlazada<List<Punto>> cuadradosCompletados = new ListaEnlazada<>();
+
+
+    
+
+    private void verificarCuadrado(Linea nuevaLinea) {
+        List<Linea> adyacentes = obtenerAdyacentes(nuevaLinea);
+        for (Linea linea1 : adyacentes) {
+            for (Linea linea2 : obtenerAdyacentes(linea1)) {
+                for (Linea linea3 : obtenerAdyacentes(linea2)) {
+                    // Verificar que las líneas son distintas
+                    if (linea1.equals(nuevaLinea) || linea2.equals(nuevaLinea) || linea3.equals(nuevaLinea)
+                        || linea1.equals(linea2) || linea1.equals(linea3) || linea2.equals(linea3)) {
+                        continue;
+                    }
+                    // Verificar que las líneas forman un cuadrado
+                    if (formaCuadrado(nuevaLinea, linea1, linea2, linea3)) {
+                        ListaEnlazada<Punto> cuadrado = new ListaEnlazada<>();
+                        agregarSiNoExiste(cuadrado, nuevaLinea.getPunto1());
+                        agregarSiNoExiste(cuadrado, nuevaLinea.getPunto2());
+                        agregarSiNoExiste(cuadrado, linea1.getPunto1());
+                        agregarSiNoExiste(cuadrado, linea1.getPunto2());
+                        agregarSiNoExiste(cuadrado, linea2.getPunto1());
+                        agregarSiNoExiste(cuadrado, linea2.getPunto2());
+                        agregarSiNoExiste(cuadrado, linea3.getPunto1());
+                        agregarSiNoExiste(cuadrado, linea3.getPunto2());
+                        if (cuadrado.getAll().size() == 4) {
+                            cuadradosCompletados.add(cuadrado.getAll());
+                            return;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+
+private List<Linea> obtenerAdyacentes(Linea linea) {
+    List<Linea> adyacentes = new ArrayList<>();
+    for(Linea l : lineasDibujadas) {
+        if(l.esAdyacente(linea) && !l.equals(linea)) {
+            adyacentes.add(l);
+        }
+    }
+    return adyacentes;
+}
+        
+    private boolean formaCuadrado(Linea l1, Linea l2, Linea l3, Linea l4) {
+    ListaEnlazada<Punto> puntos = new ListaEnlazada<>();
+    agregarSiNoExiste(puntos, l1.getPunto1());
+    agregarSiNoExiste(puntos, l1.getPunto2());
+    agregarSiNoExiste(puntos, l2.getPunto1());
+    agregarSiNoExiste(puntos, l2.getPunto2());
+    agregarSiNoExiste(puntos, l3.getPunto1());
+    agregarSiNoExiste(puntos, l3.getPunto2());
+    agregarSiNoExiste(puntos, l4.getPunto1());
+    agregarSiNoExiste(puntos, l4.getPunto2());
+    return puntos.getAll().size() == 4;
+}
+
+private void agregarSiNoExiste(ListaEnlazada<Punto> lista, Punto punto) {
+    for (Punto p : lista.getAll()) {
+        if (p.getX() == punto.getX() && p.getY() == punto.getY()) {
+            return;
+        }   
+    }
+    lista.add(punto);
+}
+
+    public PanelDePuntos(int filas, int columnas){
+
+
+        for (int i = 0; i<filas;i++){
+            for (int j = 0; j<columnas; j++){
+                Punto punto = new Punto(i*100,j*100,i,j);
+                puntosTotales.add(punto);
+            }
+        }
+
+        
+
+    
+
+        addMouseListener(new MouseAdapter(){
+            public void mouseClicked(MouseEvent e){
+                int x= e.getX();
+                int y= e.getY();
+                Punto puntoseleccionado = getPuntoCercano(x,y);
+                 if (puntoseleccionado !=null){
+                    puntosSeleccionados.add(puntoseleccionado);
+                        if (puntosSeleccionados.size()==2){
+                            Punto p1 = puntosSeleccionados.get(0);
+                            Punto p2 = puntosSeleccionados.get(1);
+
+
+                            String coordenadas = p1.toString() + "-" + p2.toString();
+                            enviarCoordenadasServidor(coordenadas);
+                            if(esLineaValida(p1,p2)){
+                                Linea linea = new Linea(p1,p2);
+                                lineasDibujadas.add(linea);
+                                verificarCuadrado(linea);
+                            }else{
+                                System.out.println("Solo se pueden hacer lineas verticales, horizontales y con un espacio de 100 entre punto");
+
+                            }
+                            puntosSeleccionados.clear();
+                            repaint();
+                                
+                            
+                            
+                        
+
+                            
+                        }
+                    
+                    
+                    
+                }
+
+                
+            }
+
+        });
+    }
+    private boolean esLineaValida(Punto p1, Punto p2) {
+    for (Linea linea : lineasDibujadas) {
+        if (linea.equals(new Linea(p1, p2))) {
+            return false;
+        }
+    }
+    return (p1.getX() == p2.getX() || p1.getY() == p2.getY()) && calcularDistancia(p1, p2) == 100;
+}
+
+    private double calcularDistancia(Punto p1, Punto p2){
+        return Math.sqrt(Math.pow(p2.getX() - p1.getX(), 2) + Math.pow(p2.getY() - p1.getY(), 2));
+    }
+
+    private Punto getPuntoCercano(int x,int y){
+        Punto puntoCercano = null;
+        double distanciaMinima = Double.MAX_VALUE;
+        for(Punto punto : puntosTotales.getAll()){
+                double distancia = Math.sqrt(Math.pow(punto.getX() - x, 2) + Math.pow(punto.getY() - y,2));
+                if (distancia<20 && distancia<distanciaMinima){
+                        distanciaMinima=distancia;
+                        puntoCercano = punto;
+                    
+                    }
+                
+            }
+            return puntoCercano;
+        }
+        
+
+
+
+
+    protected void paintComponent(Graphics g){
+    super.paintComponent(g);
+
+    g.setColor(Color.BLACK);  // Color de los puntos
+    for (Punto punto : puntosTotales.getAll()) {
+        g.fillOval(punto.getX() - 5, punto.getY() - 5, 9,9 );  // Dibuja un círculo con radio 5 en cada punto
+    }
+
+    for (List<Punto> cuadrado : cuadradosCompletados.getAll()) {     
+        cuadrado.sort((p1, p2) -> {
+            if(p1.getX() != p2.getX()) return p1.getX() - p2.getX();
+            return p1.getY() - p2.getY();
+        });
+
+        Punto p1 = cuadrado.get(0);
+        Punto p2 = cuadrado.get(1);
+        Punto p3 = cuadrado.get(2);
+        Punto p4 = cuadrado.get(3);
+
+        int[] xPoints = {p1.getX(), p2.getX(), p4.getX(), p3.getX()};
+        int[] yPoints = {p1.getY(), p2.getY(), p4.getY(), p3.getY()};
+
+        g.setColor(Color.RED);
+        g.fillPolygon(xPoints, yPoints, 4);
+    }
+
+    synchronized(puntosSeleccionados) {
+        if (puntosSeleccionados.size() == 2) {
+            puntosSeleccionados.clear();
+        }
+    }
+    for(Linea linea : lineasDibujadas) {
+        Punto p1 = linea.getPunto1();
+        Punto p2 = linea.getPunto2();
+        g.drawLine(p1.getX() + 2, p1.getY() +2 , p2.getX() + 2, p2.getY() + 2);
+        g.setColor(Color.BLACK);
+    }
+    
+
+    }
+
+
+    private void enviarCoordenadasServidor(String coordenadas){
+        try{
+
+            Socket socketclient = new Socket("localhost", 9991); // Cambia "localhost" por la dirección IP del servidor si es necesario
+            DataOutputStream dos = new DataOutputStream(socketclient.getOutputStream());
+            dos.writeUTF(coordenadas);
+            dos.close();
+            socketclient.close();
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+
+}
+
+ class Punto {
+    private int x;
+    private int y;
+
+    public Punto(int x, int y,int fila, int columna) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Getters y setters
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Punto punto = (Punto) obj;
+        return x == punto.x && y == punto.y;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}
+
+class Linea {
+    private Punto punto1;
+    private Punto punto2;
+
+    public Linea(Punto punto1, Punto punto2) {
+        this.punto1 = punto1;
+        this.punto2 = punto2;
+    }
+
+    public Punto getPunto1() {
+        return punto1;
+    }
+
+    public Punto getPunto2() {
+        return punto2;
+    }
+
+    public boolean esAdyacente(Linea otra) {
+        return this.punto1.equals(otra.punto1) || this.punto1.equals(otra.punto2) ||
+               this.punto2.equals(otra.punto1) || this.punto2.equals(otra.punto2);
+}
+
+public boolean esPerpendicular(Linea otra) {
+    if (this.punto1.getX() == this.punto2.getX()) { // Si esta línea es vertical
+        return otra.punto1.getY() == otra.punto2.getY(); // La otra debe ser horizontal
+    } else if (this.punto1.getY() == this.punto2.getY()) { // Si esta línea es horizontal
+        return otra.punto1.getX() == otra.punto2.getX(); // La otra debe ser vertical
+    }
+    return false;
+}
+public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Linea linea = (Linea) obj;
+        return (punto1.equals(linea.punto1) && punto2.equals(linea.punto2)) ||
+               (punto1.equals(linea.punto2) && punto2.equals(linea.punto1));
+    }
+}
+
+
+class Nodo<T> {
+    T data;
+    Nodo<T> next;
+
+    public Nodo(T data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+class ListaEnlazada<T> {
+    private Nodo<T> head;
+
+    public ListaEnlazada() {
+        this.head = null;
+    }
+
+    // Agregar un nuevo nodo al final
+    public void add(T data) {
+        Nodo<T> newNode = new Nodo<>(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Nodo<T> current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    // Eliminar un nodo (por el dato)
+    public void remove(T data) {
+        if (head == null) return;
+
+        if (head.data.equals(data)) {
+            head = head.next;
+            return;
+        }
+
+        Nodo<T> current = head;
+        while (current.next != null && !current.next.data.equals(data)) {
+            current = current.next;
+        }
+
+        if (current.next != null) {
+            current.next = current.next.next;
+        }
+    }
+
+    // Buscar un nodo por el dato y devolverlo
+    public Nodo<T> search(T data) {
+        Nodo<T> current = head;
+        while (current != null) {
+            if (current.data.equals(data)) {
+                return current;
+            }
+            current = current.next;
+        }
+        return null;
+    }
+
+    // Obtener todos los datos como una lista para facilitar el manejo
+    public List<T> getAll() {
+        List<T> list = new ArrayList<>();
+        Nodo<T> current = head;
+        while (current != null) {
+            list.add(current.data);
+            current = current.next;
+        }
+        return list;
     }
 }
--- ServidorProject.java
+++ ServidorProject.java
@@ -6,19 +6,12 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.ObjectOutputStream;
-import java.net.Socket;
-import java.util.ArrayList;
 
 public class ServidorProject extends JFrame {
     private ServerSocket serverSocket;
     private boolean isRunning = false;
-    private int numClientesConectados = 0; // Variable para llevar un registro de la cantidad de clientes
     private JButton startButton;
     private JButton stopButton;
-    private JTextArea logTextArea; // Agregamos un JTextArea para mostrar información del servidor
 
     public ServidorProject() {
         super("Servidor");
@@ -55,19 +48,12 @@ public class ServidorProject extends JFrame {
             }
         });
 
-        // Configuramos el JTextArea
-        logTextArea = new JTextArea();
-        logTextArea.setEditable(false); // No permitimos editar el texto
-        JScrollPane scrollPane = new JScrollPane(logTextArea);
-
         JPanel panel = new JPanel();
         panel.setLayout(new FlowLayout());
         panel.add(startButton);
         panel.add(stopButton);
 
-        // Agregamos el JTextArea al panel principal
-        add(panel, BorderLayout.NORTH);
-        add(scrollPane, BorderLayout.CENTER); // Agregamos el JTextArea con scroll al centro
+        add(panel);
 
         setVisible(true);
     }
@@ -75,21 +61,16 @@ public class ServidorProject extends JFrame {
     private void startServer() {
         if (!isRunning) {
             try {
-                int puerto = 8080; // Define el puerto aquí o en cualquier otro lugar
-                serverSocket = new ServerSocket(puerto);
+                serverSocket = new ServerSocket(8080);
                 isRunning = true;
                 startButton.setEnabled(false);
                 stopButton.setEnabled(true);
-
-                // Actualiza el JTextArea con información del servidor, incluida la cantidad de clientes
-                logTextArea.append("Servidor iniciado en el puerto " + puerto + "\n");
-                logTextArea.append("Clientes Conectados: " + numClientesConectados + "\n");
             } catch (IOException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(this, "Error al iniciar el servidor.");
             }
         }
-    }    
+    }
 
     private void stopServer() {
         if (isRunning) {
@@ -98,10 +79,6 @@ public class ServidorProject extends JFrame {
                 isRunning = false;
                 startButton.setEnabled(true);
                 stopButton.setEnabled(false);
-
-                // Actualiza el JTextArea con información del servidor, incluida la cantidad de clientes
-                logTextArea.append("Servidor detenido\n");
-                logTextArea.append("Total de clientes conectados: " + numClientesConectados + "\n");
             } catch (IOException e) {
                 e.printStackTrace();
                 JOptionPane.showMessageDialog(this, "Error al detener el servidor.");
